13350527d9bd8a10c5923d6527ca63f8606ff6ce
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-21 17:41:57 +0100

    Rewrites to get definition from template

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
   const { readFile, writeFile } = require('fs').promises
   const pdfmake = require('@alheimsins/pdf-make')
   const generateDocument = require('./lib/generate-document')
-  const definition = require('./definitions/varsel')
   const data = require('./data/varsel.json')
   const template = await readFile('templates/varsel.md', 'utf-8')
   const document = generateDocument({
-    definition,
     template,
     data
   })
