23aa3bc82e2b989cd8d2d1b6cd48dda02b1eb4eb
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-11-21 11:22:03 +0100

    Adds support for handlebars

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 (async () => {
-  const { writeFile } = require('fs').promises
+  const { readFile, writeFile } = require('fs').promises
   const pdfmake = require('@alheimsins/pdf-make')
   const generateDocument = require('./lib/generate-document')
   const definition = require('./definitions/varsel')
   const data = require('./data/varsel.json')
+  const template = await readFile('templates/varsel.hbs', 'utf-8')
   const document = generateDocument({
     definition,
-    template: {},
+    template,
     data
   })
   const buffer = await pdfmake(document)
